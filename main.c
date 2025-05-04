e1bb5be4d010ede52adaa7d6736deca9601b2e37
almAngel <alm.angel97@gmail.com>
2025-05-04 11:32:41 +0200

    Separated render and processing logic

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: angellop <angellop@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 15:54:27 by angellop          #+#    #+#             */
-/*   Updated: 2025/04/23 17:23:26 by angellop         ###   ########.fr       */
+/*   Updated: 2025/05/04 11:30:22 by angellop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,80 @@ static void ft_error(void)
 	exit(EXIT_FAILURE);
 }
 
-// static void ft_hook(void* param)
-// {
-// 	const mlx_t* mlx = param;
+static complex_t pixel_to_complex(int x, int y, camera_t *view)
+{
+	complex_t	c;
+	float		center_x;
+	float		center_y;
 
-// 	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
-// }
+	center_x = WIDTH / 2;
+	center_y = HEIGHT / 2;
+	c.re = (x - center_x) / view->zoom + view->x_offset;
+	c.im = (y - center_y) / view->zoom + view->y_offset;
+
+	return (c);
+}
+
+static int mandelbrot(complex_t c)
+{
+	complex_t	z = {0, 0};
+	int			i;
+	double		temp_re;
+
+	i = 0;
+	while (z.re * z.re + z.im * z.im <= 4 && i < MAX_ITER)
+	{
+		temp_re = z.re * z.re - z.im * z.im + c.re;
+		z.im = 2 * z.re * z.im + c.im;
+		z.re = temp_re;
+		i++;
+	}
+	return (i);
+}
+
+static int julia(complex_t c)
+{
+	complex_t	z = {0, 0};
+	int			i;
+	float		temp_re;
+
+	i = 0;
+	while (z.re * z.re + z.im * z.im <= 4 && i < MAX_ITER)
+	{
+		temp_re = z.re * z.re - z.im * z.im + c.re;
+		z.im = 2 * z.re * z.im + c.im;
+		z.re = temp_re;
+		i++;
+	}
+	return (i);
+}
+
+static void draw_fractal(mlx_image_t *img, camera_t *view)
+{
+	complex_t c;
+	int iters;
+
+	for (int y = 0; y < HEIGHT; y++)
+	{
+		for (int x = 0; x < WIDTH; x++)
+		{
+			c = pixel_to_complex(x, y, view);
+			iters = mandelbrot(c);
+			// TODO int color = get_color(iters);
+			//mlx_put_pixel(img, x, y, color);
+		}
+	}
+}
 
 int	main(void)
 {
-	mlx_set_setting(MLX_MAXIMIZED, false);
 	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "Fractol", false);
 	if (!mlx)
 		ft_error();
 	mlx_image_t* img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
 		ft_error();
-	for (int i = 0; i < WIDTH/2; i++)
-		for (int j = 0; j < HEIGHT/2; j++)
-			mlx_put_pixel(img, i, j, get_rgba(255, 255, 255, 255));
-	//mlx_loop_hook(mlx, ft_hook, mlx);
+	// draw_fractal(img);
 	mlx_loop(mlx);
 	mlx_is_key_down(mlx, MLX_KEY_ESCAPE);
 	mlx_terminate(mlx);
--- utils/includes/fractol.h
+++ utils/includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: angellop <angellop@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 17:00:22 by angellop          #+#    #+#             */
-/*   Updated: 2025/04/23 17:17:17 by angellop         ###   ########.fr       */
+/*   Updated: 2025/05/04 10:47:33 by angellop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,33 @@
 # include "MLX42/MLX42.h"
 # include <math.h>
 
-# define WIDTH 1000
-# define HEIGHT 1000
+# define WIDTH 1280
+# define HEIGHT 720
+# define MAX_ITER 100
+
+typedef struct color_s
+{
+	int	r;
+	int	g;
+	int	b;
+	int	a;
+}	color_t;
+
+typedef struct complex_s
+{
+	double	re;
+	double	im;
+}	complex_t;
+
+typedef struct divalue
+{
+
+}	pixel_t;
+
+typedef struct camera_s {
+	double	zoom;
+	double	x_offset;
+	double	y_offset;
+} camera_t;
 
 #endif
